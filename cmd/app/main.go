6670abb87d4767b9fc707ca832496c603eef63b7
samg111 <berrystump@protonmail.com>
2025-07-11 11:34:37 -0400

    feat(shared): added global var to store the resource directory

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"job-visualizer/pkg/gui"
 	"job-visualizer/pkg/headless"
+	"job-visualizer/pkg/shared"
+	"os"
 )
 
 func main() {
+	resourceDirectory, err := os.Getwd()
+	shared.CheckError(err)
+	shared.Program.ResourcesDirectory = resourceDirectory
 	isHeadless := headless.CheckCLIArguments()
 	gui.RunGUIorHeadless(isHeadless)
 }
