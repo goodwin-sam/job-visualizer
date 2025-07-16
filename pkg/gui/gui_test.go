114629fb75f39cfe15dba765bb1d426e947e2e91
samg111 <berrystump@protonmail.com>
2025-07-16 11:39:09 -0400

    refactor(tests): removed TestCreateGuiWindow function because gui cannot run in github actions

--- pkg/gui/gui_test.go
+++ pkg/gui/gui_test.go
@@ -3,29 +3,7 @@ package gui
 import (
 	"job-visualizer/pkg/shared"
 	"testing"
-
-	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/app"
 )
-
-func TestCreateGuiWindow(t *testing.T) {
-	application = app.New()
-	defer func() { application = nil }()
-
-	title := "test window"
-	window := createGuiWindow(title)
-
-	if window == nil {
-		t.Fatal("Expected window to be created, but got nil")
-	}
-	if window.Title() != title {
-		t.Errorf("Expected window title '%s', but got '%s'", title, window.Title())
-	}
-	expectedSize := fyne.NewSize(1000, 600)
-	if window.Canvas().Size() != expectedSize {
-		t.Errorf("Expected window size %v, but got %v", expectedSize, window.Canvas().Size())
-	}
-}
 
 func TestProcessJobsReturnsSlice(t *testing.T) {
 	tempDir := t.TempDir()
