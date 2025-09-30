49ae69bf6e3e54c86b769f0743abb69761108ab3
samg111 <berrystump@protonmail.com>
2025-09-30 13:51:19 -0400

    feat(build): add scripts for building an appimage and windows .exe files

--- build-appimage.sh
+++ build-appimage.sh
@@ -5,6 +5,34 @@ set -e
 
 echo "Building Job Visualizer AppImage..."
 
+# detect system architecture
+ARCH=$(uname -m)
+case $ARCH in
+    x86_64)
+        GOARCH="amd64"
+        ARCH_NAME="x86_64"
+        ;;
+    aarch64|arm64)
+        GOARCH="arm64"
+        ARCH_NAME="aarch64"
+        ;;
+    armv7l)
+        GOARCH="arm"
+        ARCH_NAME="armhf"
+        ;;
+    i386|i686)
+        GOARCH="386"
+        ARCH_NAME="i386"
+        ;;
+    *)
+        echo "Warning: Unknown architecture '$ARCH', using as-is"
+        GOARCH="$ARCH"
+        ARCH_NAME="$ARCH"
+        ;;
+esac
+
+echo "Detected architecture: $ARCH_NAME"
+
 # prompt user for version number
 read -p "Enter version number (e.g., v0.1.0): " VERSION
 
@@ -18,22 +46,22 @@ echo "Building version: $VERSION"
 
 # build the Go application
 echo "Building Go application..."
-CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o AppDir/job-visualizer-linux ./cmd/app
+CGO_ENABLED=1 GOOS=linux GOARCH=$GOARCH go build -ldflags="-s -w" -o AppDir/job-visualizer-linux ./cmd/app
 
 # make AppRun executable
 chmod +x AppDir/AppRun
 
 # create the AppImage using system appimagetool
 echo "Creating AppImage..."
-appimagetool AppDir job-visualizer-${VERSION}-x86_64.AppImage
+appimagetool AppDir job-visualizer-${VERSION}-${ARCH_NAME}.AppImage
 
 # move the created AppImage to releases directory
 echo "Moving AppImage to releases directory..."
-mv job-visualizer-${VERSION}-x86_64.AppImage releases/
+mv job-visualizer-${VERSION}-${ARCH_NAME}.AppImage releases/
 
 # make the AppImage executable
 echo "Making AppImage executable..."
-chmod +x releases/job-visualizer-${VERSION}-x86_64.AppImage
+chmod +x releases/job-visualizer-${VERSION}-${ARCH_NAME}.AppImage
 
 echo "AppImage created successfully!"
-echo "Location: releases/job-visualizer-${VERSION}-x86_64.AppImage"
+echo "Location: releases/job-visualizer-${VERSION}-${ARCH_NAME}.AppImage"
